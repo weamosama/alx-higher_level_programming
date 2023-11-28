5622a14ef05899d5d4c453915c44fa51b9edef88
= <=>
2023-11-28 10:08:44 -0800

    first

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+
+number = random.randint(-10000, 10000)
+
+last_digit = abs(number) % 10
+last_digit = -last_digit if number < 0 else last_digit
+
+print("Last digit of {}{} is {} and".format(sign, number, last_digit), end=" ")
+
+if last_digit > 5:
+    print("is greater than 5")
+elif last_digit == 0:
+    print("is 0")
+else:
+    print("is less than 6 and not 0")
