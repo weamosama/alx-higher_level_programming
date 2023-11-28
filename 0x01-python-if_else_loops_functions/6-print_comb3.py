a7e0520fd8a72e08ecebe038457fea82e7162024
= <=>
2023-11-28 10:20:26 -0800

    first

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-for i in range(100):
-    if i < 99:
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i))
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != j:
+            if i == 8 and j == 9:
+                print("{:d}{:d}".format(i, j))
+            else:
+                print("{:d}{:d}, ".format(i, j), end="")
