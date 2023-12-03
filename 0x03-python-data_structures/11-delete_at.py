66de95ebaf3bdbd38e11df02d12a429dd7540bd4
= <=>
2023-12-03 08:34:05 -0800

    tst

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     if idx >= 0 and idx < len(my_list):
-        new_list = []
-        for i in range(len(my_list)):
-            if i != idx:
-                new_list.append(my_list[i])
+        new_list = my_list[:idx] + my_list[idx + 1:]
         return new_list
     return my_list
