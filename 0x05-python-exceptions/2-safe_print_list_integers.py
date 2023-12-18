27cbf689ea5e2ed785f8b8aa3ce9264b8f46d6b9
= <=>
2023-12-18 07:09:19 -0800

    test

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,12 +5,12 @@ def safe_print_list_integers(my_list=[], x=0):
 
     try:
         for i in range(x):
-            if isinstance(my_list[i], int):
+            if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         print()
         return count
 
-    except IndexError:
+    except (IndexError, TypeError):
         print()
         return count
