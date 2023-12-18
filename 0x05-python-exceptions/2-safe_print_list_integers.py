fc8f98cc43416a0bc3693274f6b087a82f5f896e
= <=>
2023-12-18 07:11:58 -0800

    test

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
 
     try:
         for i in range(x):
-            if type(my_list[i]) == int:
+            if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         print()
