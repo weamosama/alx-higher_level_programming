6e2a393efb0fcf930990ed533147b9d88144ca49
= <=>
2023-12-18 07:19:28 -0800

    test

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError) as e:
+        print("Exception: {}".format(str(e)), file=sys.stderr)
+        return False
