90ced98941cf5a0b6103466d735fb17204ee86ec
= <=>
2023-12-19 08:59:46 -0800

    test

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module documentation goes here
+"""
+
+
+class Square:
+    """
+    Class documentation goes here
+    """
+    def __init__(self, size):
+        """
+        Method documentation goes here
+        """
+        self.__size = size
+
