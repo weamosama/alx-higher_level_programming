060cea5a24228b994ccc323f95d666b3b4534116
= <=>
2024-01-08 04:37:17 -0800

    r

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,12 +5,9 @@ Module with class MyList
 ===========================
 """
 
-
 class MyList(list):
     """Class with method print_sorted"""
-    pass
 
     def print_sorted(self):
-        """Methot that sorted a list"""
-
-        print(sorted(list(self)))
+        """Method that prints the list sorted in ascending order"""
+        print(sorted(self))
