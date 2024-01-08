08d1a5a78cd85b6d044d322a99c115665f1beaa8
= <=>
2024-01-08 04:46:24 -0800

    r

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+================================
+module with method is_same_class
+================================
+"""
+
 
 def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an instance of a_class, otherwise False."""
-    return type(obj) == a_class
+    """Method that return True if an object is an instance of a class"""
+
+    return type(obj) is a_class
