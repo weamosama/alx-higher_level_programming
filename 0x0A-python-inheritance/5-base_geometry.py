1aad7f8b132d2bf5e5ddad655dba2f4fb8115d0d
= <=>
2024-01-08 04:52:54 -0800

    r

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class BaseGeometry
+===========================
+"""
+
+
+class BaseGeometry:
+    """Empty class for representing geometric shapes."""
+    pass
