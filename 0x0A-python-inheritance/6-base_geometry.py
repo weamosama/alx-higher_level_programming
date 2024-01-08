6576dcda624c8a4f9f3a725c458802a501e814f2
= <=>
2024-01-08 04:55:09 -0800

    r

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+===========================
+Module with class BaseGeometry
+===========================
+"""
+
+
+class BaseGeometry:
+    """Class for representing geometric shapes."""
+
+    def area(self):
+        """Public instance method that raises an Exception."""
+        raise Exception("area() is not implemented")
