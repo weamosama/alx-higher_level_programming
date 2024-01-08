92f5ad41b0b545c4c351f1743e4ccc526036ef3a
= <=>
2024-01-08 04:25:59 -0800

    r

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object."""
-    return dir(obj)
+    """
+    Returns the list of available attributes and methods of an object.
+
+    :param obj: The object to inspect.
+    :return: List of attribute names.
+    """
+    return [attr for attr in dir(obj) if not callable(getattr(obj, attr))]
