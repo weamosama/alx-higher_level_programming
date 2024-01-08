009dfb2829cc8402fc615395eb1ae09633916c6e
= <=>
2024-01-08 04:48:36 -0800

    r

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -5,13 +5,14 @@ Module with method is_kind_of_class
 ======================================
 """
 
+
 def is_kind_of_class(obj, a_class):
     """
-    Method that returns True if an object is an instance of, or if the object is an instance of a class that
+    Method that returns True if an object is an instance 
     inherited from, the specified class; otherwise, False.
 
     :param obj: The object to check.
     :param a_class: The class to check against.
-    :return: True if obj is an instance of a_class or its subclass, otherwise False.
+    :return: True if obj is an instance of a_class or its subclass False.
     """
     return isinstance(obj, a_class)
