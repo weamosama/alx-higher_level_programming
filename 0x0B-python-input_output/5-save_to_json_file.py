073d76ad1b202ef8ad992c73f18eb85bc86a6ae6
= <=>
2024-01-09 10:09:36 -0800

    test

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-
+"""Module with a function to save an object to a JSON file."""
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    with open(filename, "w", encoding="UTF-8") as f:
-        json.dump(my_obj, f)
+    """Save an object to a JSON file.
+
+    Args:
+        my_obj (object): The Python object to be saved.
+        filename (str): The name of the JSON file.
+
+    Returns:
+        None
+    """
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
