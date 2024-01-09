23fa27ab1795968a90e30ca4484cf779df704733
= <=>
2024-01-09 10:23:06 -0800

    test

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""Module with a function to load an object from a JSON file."""
 
 import json
 
 
 def load_from_json_file(filename):
-    """function that creates an Object from a “JSON file”"""
-    with open(filename, "r", encoding='utf-8') as f:
-        return json.load(f)
+    """Load an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        object: The Python object loaded from the JSON file.
+    """
+    with open(filename, mode='r', encoding='utf-8') as file:
+        return json.load(file)
