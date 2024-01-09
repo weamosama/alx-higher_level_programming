6189eadda9dd752693fe48fc5245806407264386
= <=>
2024-01-09 10:06:11 -0800

    test

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""Module with a function to save an object to a text file using JSOn"""
+
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Write the JSON representation of the given object to a text file.
-
-    Args:
-        my_obj: The object to be serialized to JSON.
-        filename (str): The name of the text file to write.
-
-    Returns:
-        None
-    """
-    with open(filename, mode='w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
+    with open(filename, "w", encoding="UTF-8") as f:
+        json.dump(my_obj, f)
