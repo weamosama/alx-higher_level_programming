39dbd4eb4f5c7b4612a23a064fb775c81717a7e5
= <=>
2024-01-09 12:24:35 -0800

    test

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Class to JSON module"""
 
+
 def class_to_json(obj):
     """
-    Returns the dictionary description with simple data structure for JSON serialization of an object.
+    Returns the dictionary description with simple dat.
     """
 
     if hasattr(obj, "__dict__"):
