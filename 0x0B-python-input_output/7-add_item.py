ed7b8edc649b5eb916cc87fb4df8935360272452
= <=>
2024-01-09 08:24:36 -0800

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-"""Script to add command-line arguments."""
 
-import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
 
-filename = "add_item.json"
+import json
 
-try:
-    # Load existing data from the file
-    my_list = load_from_json_file(filename)
-except (FileNotFoundError, ValueError):
-    # If the file doesn't exist or
-    my_list = []
 
-# Add command-line arguments to the list
-my_list.extend(sys.argv[1:])
-
-# Save the updated list to the file
-save_to_json_file(my_list, filename)
+def save_to_json_file(my_obj, filename):
+    with open(filename, "w", encoding="UTF-8") as f:
+        json.dump(my_obj, f)
