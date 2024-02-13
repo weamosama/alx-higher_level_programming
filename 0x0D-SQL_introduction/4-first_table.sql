09376bb3292da29315d1ac0d71da3d98acdcdcb9
= <=>
2024-02-13 10:52:26 -0800

    test

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,8 +1,6 @@
 -- 4. First table
 -- This script creates a table called first_table in the current database
 
-USE hbtn_0c_0;
-
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
