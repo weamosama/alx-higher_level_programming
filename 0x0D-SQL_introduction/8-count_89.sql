985f6bd2c293f485b8051b5af595207c817a8f01
= <=>
2024-02-13 11:01:31 -0800

    test

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- 8. Count records with id = 89 in first_table
+-- This script displays the number of records with id = 89 in the table first_table
+
+SELECT COUNT(*) FROM first_table WHERE id = 89;
