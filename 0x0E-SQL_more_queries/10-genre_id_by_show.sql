c60bb7c56c01e549349a21038a0b763d4b54e983
= <=>
2024-02-14 11:10:12 -0800

    test

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,6 @@
 -- 10-genre_id_by_show.sql
 
 -- Use the 'hbtn_0d_tvshows' database
-USE hbtn_0d_tvshows;
-
 -- Select all shows with at least one linked genre
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
