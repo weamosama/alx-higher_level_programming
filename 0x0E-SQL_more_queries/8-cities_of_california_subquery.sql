e26b1615664e3c106bbd7e6495f35ec801f51d3a
= <=>
2024-02-14 11:07:39 -0800

    test

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,5 @@
 -- 8-cities_of_california_subquery.sql
 
--- Use the 'hbtn_0d_usa' database
-USE hbtn_0d_usa;
-
 -- Select all cities of California using a subquery
 SELECT * FROM cities
 WHERE state_id = (
