841ed99a881ae9bd7bba13b99b19845c9bd84bdf
= <=>
2024-02-14 11:14:58 -0800

    test

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,12 @@
+-- 14-my_genres.sql
+
+-- Use the 'hbtn_0d_tvshows' database
+USE hbtn_0d_tvshows;
+
+-- Select all genres of the show Dexter
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name ASC;
