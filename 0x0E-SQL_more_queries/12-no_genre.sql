77b8d663544370df479e2ad7b5f2a976bb80f060
= <=>
2024-02-15 03:48:19 -0800

    test

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,11 +1,10 @@
--- 12-no_genre.sql
+-- lists all shows contained in hbtn_0d_tvshows without a genre linked
+   -- Each record should display: tv_shows.title - tv_show_genres.genre_id
+   -- Results must be sorted in ascending order by tv_shows.title
+   -- and tv_show_genres.genre_id
+   -- The database name will be passed as an argument of the mysql command
 
--- Use the 'hbtn_0d_tvshows' database
-USE hbtn_0d_tvshows;
-
--- Select all shows without a linked genre
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+SELECT title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
+WHERE tv_show_genres.show_id IS NULL
+ORDER BY title, tv_show_genres.genre_id;
