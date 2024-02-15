27b4fc9c04112603d7626682b0f1b7de85d97806
= <=>
2024-02-15 03:47:08 -0800

    test

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,11 +1,10 @@
--- 11-genre_id_all_shows.sql
+-- lists all shows contained in the database hbtn_0d_tvshows
+   -- Each record should display: tv_shows.title - tv_show_genres.genre_id
+   -- Results must be sorted in ascending order by tv_shows.title
+   -- and tv_show_genres.genre_id
+   -- If a show doesn’t have a genre, display NULL
+   -- The database name will be passed as an argument of the mysql command
 
--- Use the 'hbtn_0d_tvshows' database
-USE hbtn_0d_tvshows;
-
--- Select all shows and their linked genres (or NULL if no genre)
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
-
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
