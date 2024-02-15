1de4f55e52085ef5f280c3c8e984470040feff50
= <=>
2024-02-15 03:53:01 -0800

    test

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,13 +1,12 @@
--- 15-comedy_only.sql
+-- lists all Comedy shows in the database hbtn_0d_tvshows
+-- displays the number of shows linked to each
+   -- Each record should display:
+      -- tv_shows.title
+   -- Results must be sorted in ascending order by the show title
+   -- The database name will be passed as an argument of the mysql command
 
--- Use the 'hbtn_0d_tvshows' database
-USE hbtn_0d_tvshows;
-
--- Select all Comedy shows
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+SELECT title FROM tv_shows
+JOIN tv_show_genres ON id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
-
+ORDER BY title;
--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,12 +1,11 @@
--- 16-shows_by_genre.sql
+-- lists all shows and linked genres from the database hbtn_0d_tvshows
+   -- If a show doesn’t have a genre, display NULL in the genre column
+   -- Each record should display:
+      -- tv_shows.title - tv_genres.name
+   -- Results must be sorted in ascending order by the show title and genre
+   -- The database name will be passed as an argument of the mysql command
 
--- Use the 'hbtn_0d_tvshows' database
-USE hbtn_0d_tvshows;
-
--- Select shows and their linked genres
-SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+SELECT title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_show_genres ON id=tv_show_genres.show_id
 LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ASC;
-
+ORDER BY title, tv_genres.name;
